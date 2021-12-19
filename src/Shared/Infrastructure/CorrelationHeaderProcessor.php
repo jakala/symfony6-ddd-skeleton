7004b1db7b0d3ce6b26ce1a34cef18d45738280d
Angel Cid <angel.cid@gmail.com>
2021-12-19 23:41:11 +0100

    Cambio el nombre de MonologRequestProcessor por CorrelationHeaderProcessor, que tiene mas sentido a nivel de nuestra app

--- src/Shared/Infrastructure/CorrelationHeaderProcessor.php
+++ src/Shared/Infrastructure/CorrelationHeaderProcessor.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Shared\Infrastructure;
 
-final class MonologRequestProcessor
+final class CorrelationHeaderProcessor
 {
     private ?string $token = null ;
     private ?int $count = null ;
